cee16ea678174405c5730f4eb272b88aff1c77bc
shalom <shalom938@gmail.com>
2022-01-01 06:07:35 +0200

    change package names

--- message-journal/src/main/java/module-info.java
+++ message-journal/src/main/java/module-info.java
@@ -1,6 +1,6 @@
-module messages.journal {
+module sample.messages.journal {
     requires utilities;
     requires transitive sample.journal.all;
     requires com.google.common;
-    exports org.sk.sample.messagejournal;
+    exports org.sk.sample.messagejournal to sample.messenger;
 }
--- messenger/src/main/java/module-info.java
+++ messenger/src/main/java/module-info.java
@@ -1,9 +1,9 @@
 import org.sk.sample.messenger.spi.MessageProvider;
 
 module sample.messenger {
-    exports org.sk.sample.messenger;
+    exports org.sk.sample.messenger to sample.app;
     exports org.sk.sample.messenger.spi;
     requires org.apache.commons.lang3;
-    requires messages.journal;
+    requires sample.messages.journal;
     uses MessageProvider;
 }
