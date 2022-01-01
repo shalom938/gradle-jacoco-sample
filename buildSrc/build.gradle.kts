7e4b083d487bcfcab6b8373d48bd9ccab404cdf7
shalom <shalom938@gmail.com>
2022-01-01 06:36:04 +0200

    build src

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    `groovy-gradle-plugin`
+    `java-gradle-plugin`
+}
+
+
+//gradlePlugin {
+//    plugins {
+//        create("SamplePlugin") {
+//            id = "org.sk.sample.plugin.SamplePlugin"
+//            implementationClass = "org.sk.sample.plugin.SamplePlugin"
+//        }
+//    }
+//}
