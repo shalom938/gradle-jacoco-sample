59feaef93d786d9f2b718d9bf1e9c283b853b09b
shalom <shalom938@gmail.com>
2022-01-02 21:53:02 +0200

    comments

--- journals/journals-all/src/main/java/module-info.java
+++ journals/journals-all/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module sample.journal.all {
     //its transitive so users of this module can directly use classes in sample.journal
     //without the need to require it in their module-info. it's like saying 'who ever requires this module can
     //also access the api exposed by sample.journal as if they required it'
+    //it corresponds to gradle 'api' configuration
     requires transitive sample.journal;
     requires sample.journal.impl;
     requires org.apache.commons.lang3;
